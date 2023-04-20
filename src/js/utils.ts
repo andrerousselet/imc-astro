d9bb7dd7761d10be3d2fb90031ed3626b86020e9
Andre Rousselet <andre098@hotmail.com>
2023-04-20 11:13:31 -0300

    style(utils): remove comments

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -14,33 +14,3 @@ export function formatDate(date: string, timeZone: string = 'UTC'): string {
   };
   return new Date(date).toLocaleDateString("pt-BR", options);
 }
-
-// export function formatBlogPosts(
-//   posts,
-//   {
-//     filterOutDrafts = true,
-//     filterOutFuturePosts = true,
-//     sortByDate = true,
-//     limit = undefined,
-//   } = {}
-// ) {
-//   const filteredPosts = posts.reduce((acc, curr) => {
-//     const { date, draft } = curr.frontmatter;
-//     if (filterOutDrafts && draft) return acc;
-//     if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
-//     acc.push(curr);
-//     return acc;
-//   }, [])
-
-//   if (sortByDate) {
-//     filteredPosts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
-//   } else {
-//     filteredPosts.sort(() => Math.random() - 0.5)
-//   }
-
-//   if (typeof limit === 'number') {
-//     return filteredPosts.slice(0, limit);
-//   }
-
-//   return filteredPosts;
-// }
